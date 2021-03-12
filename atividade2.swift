0e596ac75bc1fc94d41a4d9e9a13dfabd7ad8ac5
viniiciusbeniites <vinicius.benites02@hotmail.com>
2021-03-12 09:52:48 -0300

    2 atividade swift

---
+++ atividade2.swift
@@ -0,0 +1,34 @@
+//Questão 1
+//If= passa pela condição primeiro, e SE ela for verdadeira, executa a ação.
+//Guard= faz a saída do ELSE primeiro, para depois verificar a condição, e aí sim, realizá-la.
+
+//Questão 2
+  var num1 = 10
+  var num2 = 5
+  var result = "Este número \(num1) acompanhado deste outro \(num2)"
+  func lancaDados (){
+      print(result)
+  }
+
+
+//Questão 3
+func logar(){
+  let login: String? = "user"
+  let senha: String? = "senha"
+
+  guard let login = login && senha = senha{
+      else {
+        print ("Dados incorretos")
+        }
+      let login == "user" && senha == "senha"
+      {
+        print("Login e Senha corretos")
+      }
+    }
+
+
+//Questão 4
+var range = 200..500
+range.contains()
+
+
